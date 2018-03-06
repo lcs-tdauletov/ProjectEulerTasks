3ff8d6372ead54085489e25adcb8abd00efc60c6
Russell Gordon <rgordon@lcs-mba2410.lcs.on.ca>
2018-03-06 12:18:40 -0500

    Initial Commit

---
+++ ProjectEulerTasks/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ProjectEulerTasks
+//
+//  Created by Gordon, Russell on 2018-03-06.
+//  Copyright © 2018 Gordon, Russell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
