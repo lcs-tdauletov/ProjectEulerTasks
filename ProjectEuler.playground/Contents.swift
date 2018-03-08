39bdf7ff5c99f249f652e15adcaf656f9c71457d
lcs-tdauletov <32198299+lcs-tdauletov@users.noreply.github.com>
2018-03-08 09:40:25 -0500

    This is the best thing ever.(BELL) (First euler challenger)

--- ProjectEuler.playground/Contents.swift
+++ ProjectEuler.playground/Contents.swift
@@ -3,3 +3,19 @@
 import Cocoa
 
 var str = "Hello, playground"
+
+var divisibleInteger : [Int] = []
+var total = 0
+for i in 1...1001 {
+    if i % 3 == 0 || i % 5 == 0{
+        divisibleInteger.append(i)
+    }
+}
+divisibleInteger
+for z in 0...divisibleInteger.count-1 {
+    total += divisibleInteger[z]
+}
+
+print(total)
+
+
